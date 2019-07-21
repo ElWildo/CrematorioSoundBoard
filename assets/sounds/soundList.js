fc4b52c9b29e945c0f8af5698c38af576bfe4837
Francesco Wild <francescowild90@gmail.com>
2019-07-21 17:22:19 +0100

    update mp3 files

---
+++ assets/sounds/soundList.js
@@ -0,0 +1,140 @@
+const soundsList = [{
+    name: "2_madonne",
+    link: require("./2_madonne.mp3")
+},
+{
+    name: "ban_zed",
+    link: require("./ban_zed.mp3")
+},
+{
+    name: "bestemmione_2k19",
+    link: require("./bestemmione_2k19.mp3")
+},
+{
+    name: "brutto_omofobico_del_dioporco",
+    link: require("./brutto_omofobico_del_dioporco.mp3")
+},
+{
+    name: "cane_abbaia",
+    link: require("./cane_abbaia.mp3")
+},
+{
+    name: "condotta",
+    link: require("./condotta.mp3")
+},
+{
+    name: "danni_team",
+    link: require("./danni_team.mp3")
+},
+{
+    name: "datemi_una_mano",
+    link: require("./datemi_una_mano.mp3")
+},
+{
+    name: "dio_can_mi_fai_venire_il_dio_can",
+    link: require("./dio_can_mi_fai_venire_il_dio_can.mp3")
+},
+{
+    name: "fiero",
+    link: require("./fiero.mp3")
+},
+{
+    name: "forte_come_i_negri",
+    link: require("./forte_come_i_negri.mp3")
+},
+{
+    name: "gli_morisse_la_m",
+    link: require("./gli_morisse_la_m.mp3")
+},
+{
+    name: "gol_nonna",
+    link: require("./gol_nonna.mp3")
+},
+{
+    name: "insultato_fino_a_poco_fa",
+    link: require("./insultato_fino_a_poco_fa.mp3")
+},
+{
+    name: "lasciatemi_vivo",
+    link: require("./lasciatemi_vivo.mp3")
+},
+{
+    name: "licantropo",
+    link: require("./licantropo.mp3")
+},
+{
+    name: "maria_teresa",
+    link: require("./maria_teresa.mp3")
+},
+{
+    name: "merda",
+    link: require("./merda.mp3")
+},
+{
+    name: "nice_guy_tommy",
+    link: require("./nice_guy_tommy.mp3")
+},
+{
+    name: "non_preme_la_e",
+    link: require("./non_preme_la_e.mp3")
+},
+{
+    name: "patagonia",
+    link: require("./patagonia.mp3")
+},
+{
+    name: "pentakill",
+    link: require("./pentakill.mp3")
+},
+{
+    name: "Potenza_pugno",
+    link: require("./Potenza_pugno.mp3")
+},
+{
+    name: "quanto_sei_disabile_mentalmente",
+    link: require("./quanto_sei_disabile_mentalmente.mp3")
+},
+{
+    name: "roba",
+    link: require("./roba.mp3")
+},
+{
+    name: "scarafaggio",
+    link: require("./scarafaggio.mp3")
+},
+{
+    name: "sei_un_fallito_con_gli_occhiali",
+    link: require("./sei_un_fallito_con_gli_occhiali.mp3")
+},
+{
+    name: "sei_un_fallito_nella_vita_andrai_a_succhiare_i_cazzi",
+    link: require("./sei_un_fallito_nella_vita_andrai_a_succhiare_i_cazzi.mp3")
+},
+{
+    name: "sei_un_leso_stai_zitto",
+    link: require("./sei_un_leso_stai_zitto.mp3")
+},
+{
+    name: "televisore_esplode",
+    link: require("./televisore_esplode.mp3")
+},
+{
+    name: "ti_sto_dicendo",
+    link: require("./ti_sto_dicendo.mp3")
+},
+{
+    name: "tumore_ai_denti",
+    link: require("./tumore_ai_denti.mp3")
+},
+{
+    name: "una_sega_con_il_lampadario",
+    link: require("./una_sega_con_il_lampadario.mp3")
+},
+{
+    name: "veramente",
+    link: require("./veramente.mp3")
+},
+{
+    name: "volontariato",
+    link: require("./volontariato.mp3")
+}]
